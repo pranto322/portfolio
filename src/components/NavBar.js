9fc38cde6d4fe90936427fbf7d5fc20b841c689f
pranto322 <parijatsaha435@gmail.com>
2023-07-27 15:30:43 +0600

    git commit -m "modified nav"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
+import classNames from 'classnames';
 
 const NavBar = () => {
-  const [activeLink, setActiveLink] = useState('');
+  
   const links = [
     {
       id: 1,
       link: 'home',
-      isActive: activeLink === 'home',
     },
     {
       id: 2,
       link: 'about',
-      isActive: activeLink === 'about',
     },
     {
       id: 3,
       link: 'project',
-      isActive: activeLink === 'project',
     },
     {
       id: 4,
       link: 'contact',
-      isActive: activeLink === 'contact',
     },
   ];
-
-  const [nav, setNav] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -39,32 +34,27 @@ const NavBar = () => {
     handleResize();
     window.addEventListener('resize', handleResize);
 
-  
-    const storedActiveLink = localStorage.getItem('activeLink');
-    setActiveLink(storedActiveLink || 'home');
-
-   
-    if (storedActiveLink && storedActiveLink !== 'home') {
-      const element = document.getElementById(storedActiveLink);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
+    const handleBeforeUnload = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'auto',
+      });
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
 
     return () => {
       window.removeEventListener('resize', handleResize);
+      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
 
-
-  const handleSetActiveLink = (link) => {
-    setActiveLink(link);
-    localStorage.setItem('activeLink', link);
-  };
+  const [nav, setNav] = useState(false);
 
   const toggleNav = () => {
     setNav(!nav);
   };
+
+
 
   return (
     <>
@@ -76,28 +66,38 @@ const NavBar = () => {
           {links.map((link) => (
             <li
               key={link.id}
-              className={`p-4 cursor-pointer capitalize font-semibold hover:scale-110 duration-200 ${link.isActive ? 'underline decoration-lime-400 decoration-4 underline-offset-8' : ''}`}
+              className={classNames('p-4 cursor-pointer capitalize font-semibold hover:scale-110 duration-200')}
             >
-              
-              <Link to={link.link} smooth duration={500} offset={-20} onClick={() => handleSetActiveLink(link.link)}>
+              <Link
+                to={link.link}
+                smooth
+                duration={500}
+                offset={-20}
+                
+              >
                 {link.link}
               </Link>
             </li>
           ))}
         </ul>
         <div onClick={toggleNav} className='cursor-pointer pr-4 z-10 md:hidden'>
-          {nav ? <i className="uil uil-times text-2xl"></i> : <i className="uil uil-bars text-2xl"></i>}
+          {nav ? <i className='uil uil-times text-2xl'></i> : <i className='uil uil-bars text-2xl'></i>}
         </div>
       </div>
-      {nav && <div className="w-full h-36 md:hidden" />}
+      {nav && <div className='w-full h-36 md:hidden' />}
       <ul className={`flex flex-col justify-center items-center absolute top-20 left-0 w-full md:hidden bg-white z-30 ${nav ? 'block' : 'hidden'}`}>
         {links.map((link) => (
           <li
             key={link.id}
-            className={`px-4 cursor-pointer capitalize  hover:scale-110 duration-200 font-semibold py-2 ${link.isActive ? 'underline decoration-lime-400 decoration-4 underline-offset-8' : ''}`}
+            className={classNames('px-4 cursor-pointer capitalize  hover:scale-110 duration-200 font-semibold py-2')}
           >
-           
-            <Link to={link.link} smooth duration={500} offset={-20} onClick={() => handleSetActiveLink(link.link)}>
+            <Link
+              to={link.link}
+              smooth
+              duration={500}
+              offset={-20}
+              
+            >
               {link.link}
             </Link>
           </li>
